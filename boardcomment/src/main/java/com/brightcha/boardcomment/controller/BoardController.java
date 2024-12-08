883d69fe37b7d67b18f92c04628685e1ca2e7a04
brightCha <ckguscjf03@gmail.com>
2024-12-08 21:23:18 +0900

    📚Docs: Swagger description write

--- boardcomment/src/main/java/com/brightcha/boardcomment/controller/BoardController.java
+++ boardcomment/src/main/java/com/brightcha/boardcomment/controller/BoardController.java
@@ -3,6 +3,10 @@ package com.brightcha.boardcomment.controller;
 import com.brightcha.boardcomment.dto.BoardRequestDto;
 import com.brightcha.boardcomment.dto.BoardResponseDto;
 import com.brightcha.boardcomment.service.BoardService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -14,31 +18,62 @@ import java.util.List;
 @Slf4j
 @RequestMapping("/api")
 @RequiredArgsConstructor
+@Tag(name = "BoardController", description = "게시판 관련 API를 제공합니다.")
 public class BoardController {
 
     private final BoardService boardService;
 
     @GetMapping("/boards")
+    @Operation(summary = "게시글 전체 조회", description = "모든 게시글을 조회하는 API")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "성공적으로 모든 게시글을 조회함."),
+            @ApiResponse(responseCode = "500", description = "서버 에러 발생.")
+    })
     public ResponseEntity<List<BoardResponseDto>> getBoards() {
         return ResponseEntity.ok(boardService.getBoards());
     }
 
     @GetMapping("/boards/{boardId}")
+    @Operation(summary = "특정 게시글 조회", description = "boardId에 해당하는 게시글을 조회하는 API")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "성공적으로 특정 게시글을 조회함."),
+            @ApiResponse(responseCode = "404", description = "해당 게시글을 찾을 수 없음."),
+            @ApiResponse(responseCode = "500", description = "서버 에러 발생.")
+    })
     public ResponseEntity<BoardResponseDto> getBoard(@PathVariable Long boardId) {
         return ResponseEntity.ok(boardService.getBoard(boardId));
     }
 
     @PostMapping("/boards")
+    @Operation(summary = "게시글 생성", description = "새로운 게시글을 생성하는 API")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "성공적으로 게시글을 생성함."),
+            @ApiResponse(responseCode = "400", description = "입력값이 잘못되었음."),
+            @ApiResponse(responseCode = "500", description = "서버 에러 발생.")
+    })
     public ResponseEntity<BoardResponseDto> createBoard(@RequestBody BoardRequestDto boardRequestDto) {
         return ResponseEntity.ok(boardService.createBoard(boardRequestDto));
     }
 
     @PutMapping("/boards/{boardId}")
+    @Operation(summary = "게시글 수정", description = "boardId에 해당하는 게시글을 수정하는 API")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "성공적으로 게시글을 수정함."),
+            @ApiResponse(responseCode = "404", description = "해당 게시글을 찾을 수 없음."),
+            @ApiResponse(responseCode = "400", description = "입력값이 잘못되었음."),
+            @ApiResponse(responseCode = "500", description = "서버 에러 발생.")
+    })
     public ResponseEntity<BoardResponseDto> updateBoard(@PathVariable Long boardId, @RequestBody BoardRequestDto boardRequestDto) {
         return ResponseEntity.ok(boardService.updateBoard(boardId, boardRequestDto));
     }
 
     @DeleteMapping("/boards/{boardId}")
+    @Operation(summary = "게시글 삭제", description = "boardId에 해당하는 게시글을 삭제하는 API")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "성공적으로 게시글을 삭제함."),
+            @ApiResponse(responseCode = "404", description = "해당 게시글을 찾을 수 없음."),
+            @ApiResponse(responseCode = "500", description = "서버 에러 발생.")
+    })
     public ResponseEntity<String> deleteBoard(@PathVariable Long boardId) {
         boardService.deleteBoard(boardId);
         return ResponseEntity.ok("SUCCESS");
--- boardcomment/src/main/java/com/brightcha/boardcomment/controller/CommentController.java
+++ boardcomment/src/main/java/com/brightcha/boardcomment/controller/CommentController.java
@@ -4,6 +4,10 @@ import com.brightcha.boardcomment.dto.CommentRequestDto;
 import com.brightcha.boardcomment.dto.CommentResponseDto;
 import com.brightcha.boardcomment.dto.CommentUpdateRequestDto;
 import com.brightcha.boardcomment.service.CommentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -15,26 +19,53 @@ import java.util.List;
 @RequiredArgsConstructor
 @Slf4j
 @RequestMapping("/api")
+@Tag(name = "CommentController", description = "댓글 관련 API를 제공합니다.")
 public class CommentController {
 
     private final CommentService commentService;
 
     @GetMapping("/boards/{boardId}/comments")
+    @Operation(summary = "댓글 목록 조회", description = "특정 게시글(boardId)에 대한 모든 댓글을 조회하는 API")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "성공적으로 댓글 목록을 조회함."),
+            @ApiResponse(responseCode = "404", description = "해당 게시글을 찾을 수 없음."),
+            @ApiResponse(responseCode = "500", description = "서버 에러 발생.")
+    })
     public ResponseEntity<List<CommentResponseDto>> getComments(@PathVariable Long boardId) {
         return ResponseEntity.ok(commentService.getComments(boardId));
     }
 
     @PostMapping("/boards/{boardId}/comments")
+    @Operation(summary = "댓글 생성", description = "특정 게시글(boardId)에 새로운 댓글을 생성하는 API")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "성공적으로 댓글을 생성함."),
+            @ApiResponse(responseCode = "400", description = "입력값이 잘못되었음."),
+            @ApiResponse(responseCode = "404", description = "해당 게시글을 찾을 수 없음."),
+            @ApiResponse(responseCode = "500", description = "서버 에러 발생.")
+    })
     public ResponseEntity<CommentResponseDto> createComment(@PathVariable Long boardId, @RequestBody CommentRequestDto commentDto) {
         return ResponseEntity.ok(commentService.createComment(boardId, commentDto));
     }
 
     @PutMapping("/comments/{commentId}")
+    @Operation(summary = "댓글 수정", description = "특정 댓글(commentId)을 수정하는 API")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "성공적으로 댓글을 수정함."),
+            @ApiResponse(responseCode = "404", description = "해당 댓글을 찾을 수 없음."),
+            @ApiResponse(responseCode = "400", description = "입력값이 잘못되었음."),
+            @ApiResponse(responseCode = "500", description = "서버 에러 발생.")
+    })
     public ResponseEntity<CommentResponseDto> updateComment(@PathVariable Long commentId, @RequestBody CommentUpdateRequestDto commentUpdateRequestDto) {
         return ResponseEntity.ok(commentService.updateComment(commentId, commentUpdateRequestDto));
     }
 
     @DeleteMapping("/comments/{commentId}")
+    @Operation(summary = "댓글 삭제", description = "특정 댓글(commentId)을 삭제하는 API")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "성공적으로 댓글을 삭제함."),
+            @ApiResponse(responseCode = "404", description = "해당 댓글을 찾을 수 없음."),
+            @ApiResponse(responseCode = "500", description = "서버 에러 발생.")
+    })
     public ResponseEntity<String> deleteComment(@PathVariable Long commentId) {
         commentService.deleteComment(commentId);
         return ResponseEntity.ok("SUCCESS");
