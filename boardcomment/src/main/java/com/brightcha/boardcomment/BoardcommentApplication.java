790cbe9809a900854927bace714261f3493c3efc
brightCha <ckguscjf03@gmail.com>
2024-12-08 16:15:14 +0900

    🎉Init: project create

---
+++ boardcomment/src/main/java/com/brightcha/boardcomment/BoardcommentApplication.java
@@ -0,0 +1,13 @@
+package com.brightcha.boardcomment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardcommentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardcommentApplication.class, args);
+	}
+
+}
---
+++ boardcomment/src/test/java/com/brightcha/boardcomment/BoardcommentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brightcha.boardcomment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardcommentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
